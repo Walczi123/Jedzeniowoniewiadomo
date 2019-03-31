4be9ca56e40f5e5d267b468713ab158aefc066d9
Patryk <38495867+Walczi123@users.noreply.github.com>
2019-03-31 18:08:18 +0200

    Recipe update

--- Java/Database.java
+++ Java/Database.java
@@ -57,4 +57,96 @@ public class Database {
             return result;
         }
     }
+    void insertRecipe(Recipe input){
+        //connecting
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "user", "password")) {
+            //SQL command
+            String insertSQLRecipe = "INSERT INTO recipes (name, description, user_id) VALUES (?,?,?)";
+            String insertSQLProduct_recipe = "INSERT INTO products_recipes (product_id, recipe_id, amount, unit_id) VALUES (?,?,1,1)";
+            PreparedStatement statementRecipe = connection.prepareStatement(insertSQLRecipe),statementProduct_recipe = connection.prepareStatement(insertSQLProduct_recipe);
+            //input variables in statement for table recipe
+            statementRecipe.setString(1, input.getName());
+            statementRecipe.setString(2, input.getDescription());
+            statementRecipe.setInt(3, input.getUserId());
+            //execute SQL statement for table recipe
+            //statementRecipe.executeUpdate();
+            //getting id of added record
+/*
+            long recordId= 0;
+            int affectedRows = statementRecipe.executeUpdate();
+            // check the affected rows
+            if (affectedRows > 0) {
+                // get the ID back
+                try (ResultSet rs = statementRecipe.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        recordId = rs.getLong(1);
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+*/          long recordId= 0;
+            int i=statementRecipe.executeUpdate();
+            System.out.println(i+" records inserted");
+            if (i > 0) {
+                // get the ID back
+                try (ResultSet rs = statementRecipe.getGeneratedKeys()) {
+
+                        rs.next();rs.next();
+                        System.out.println(rs);
+                        recordId = rs.getLong(1);
+
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            System.out.println(recordId);
+            //input variables in statement for table product_recipe
+/*            for(var record:input.getProductsList()){
+                statementProduct_recipe.setInt(1,record);
+                statementProduct_recipe.setInt(2,recordId);
+                //execute SQL statement for table product_recipe in every iteration
+                statementProduct_recipe.executeUpdate();
+            }*/
+        }
+        catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+    void deleteCascadeRecipe(int recipeId){
+        //connecting
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "user", "password")) {
+            //Query 1
+            PreparedStatement statement=connection.prepareStatement("DELETE FROM recipes WHERE recipe_id=?");
+            statement.setInt(1, recipeId);
+            //execute the delete statement
+            statement.executeUpdate();
+            }
+        catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+    void updateRecipe(int recipeId, Recipe recipe){
+        //connecting
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "user", "password")) {
+            //Query 1
+            PreparedStatement statement=connection.prepareStatement("UPDATE recipes SET name=?,description=?,user_id=? WHERE recipe_id=?");
+            statement.setString(1,recipe.getName());
+            statement.setString(2,recipe.getDescription());
+            statement.setInt(3,recipe.getUserId());
+            statement.setInt(4, recipeId);
+            //execute the delete statement
+            statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
