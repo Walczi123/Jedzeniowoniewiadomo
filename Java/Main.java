94fa71967c7ac2843a7bbc822f0608b2932091ea
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-04-15 16:43:34 +0200

    recipeProvider_0_5

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,23 @@
+import Models.Recipe;
+import SQL.PostgresDatabaseProvider;
+import SQL.RecipeSQLProvider;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        try {
+            PostgresDatabaseProvider databaseLoader = new PostgresDatabaseProvider("localhost:5432/Jedzenioweniewiadomo", "user", "password");
+            databaseLoader.open();
+            RecipeSQLProvider rp = new RecipeSQLProvider(databaseLoader);
+            List<Recipe> recipeList = rp.getAllUserRecipe(10);
+            for (var a:recipeList) {
+                System.out.println(a.getName());
+            }
+            databaseLoader.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- Java/SQL/RecipeSQLProvider.java
+++ Java/SQL/RecipeSQLProvider.java
@@ -4,36 +4,56 @@ import Models.Recipe;
 import SQLTablesNames.RecipeTable;
 import SQLTablesNames.ProductsRecipesTable;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RecipeSQLProvider {
-    private DatabaseProvider datbaseLoader;
+    private DatabaseProvider datbaseProvider;
 
     public  RecipeSQLProvider(DatabaseProvider databaseProvider) {
-        this.datbaseLoader = databaseProvider;
+        this.datbaseProvider = databaseProvider;
     }
 
-    Recipe getRecipe(int recipeId) throws SQLException {
+    public Recipe getRecipe(int recipeId) throws SQLException {
+        //DESCRIPTION: return recipe of given id (recipeId)
         Recipe result=new Recipe();
-        Connection c = datbaseLoader.getConnection();
+        Connection c = datbaseProvider.getConnection();
+        //Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Jedzenioweniewiadomo", "user", "password");
         //Query 1
-        var stmt_rec = c.prepareStatement("SELECT * FROM "+ RecipeTable.TABLE_NAME +"WHERE "+RecipeTable.USER_ID+"=?");
+        var stmt_rec = c.prepareStatement("SELECT * FROM "+ RecipeTable.TABLE_NAME +" WHERE "+RecipeTable.RECIPE_ID+"=?");
         stmt_rec.setInt(1, recipeId);
         //Query 2
-        var stmt_prod = c.prepareStatement("SELECT * FROM "+ ProductsRecipesTable.TABLE_NAME +" WHERE"+RecipeTable.USER_ID+"=?");
+        var stmt_prod = c.prepareStatement("SELECT * FROM "+ ProductsRecipesTable.TABLE_NAME +" WHERE "+ProductsRecipesTable.RECIPE_ID+"=?");
         stmt_prod.setInt(1, recipeId);
         //Execute Queries
         var result_rec = stmt_rec.executeQuery();
         var result_prod= stmt_prod.executeQuery();
         //Save data
         result_rec.next();
-        result.setName(result_rec.getString("name"));
-        result.setDescription(result_rec.getString("description"));
-        result.setUserId(result_rec.getInt("user_id"));
+        result.setName(result_rec.getString(RecipeTable.NAME));
+        result.setDescription(result_rec.getString(RecipeTable.DESCRIPTION));
+        result.setUserId(result_rec.getInt(RecipeTable.USER_ID));
         while (result_prod.next()) {
-            result.addProduct(result_prod.getInt("product_id"));
+            result.addProduct(result_prod.getInt(ProductsRecipesTable.PRODUCT_ID));
+        }
+        return result;
+    }
+
+    public List<Recipe> getAllUserRecipe(int userId) throws SQLException{
+        //DESCRIPTION: return all recipes of user (userId)
+        List<Recipe> result= new ArrayList<Recipe>();
+        //connecting
+        Connection c = datbaseProvider.getConnection();
+        //Query
+        var stmt = c.prepareStatement("SELECT * FROM "+RecipeTable.TABLE_NAME+" WHERE "+RecipeTable.USER_ID+"=?");
+        stmt.setInt(1, userId);
+        //Execute Query
+        var r = stmt.executeQuery();
+        //Save data
+        while (r.next()) {
+            result.add(getRecipe(r.getInt(RecipeTable.RECIPE_ID)));
         }
         return result;
     }
